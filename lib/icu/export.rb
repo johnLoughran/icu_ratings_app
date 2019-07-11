c0f96b231a5b2318e5a13c8feb726628fd81262d
johnLoughran <52636380+johnLoughran@users.noreply.github.com>
2019-07-12 00:54:53 +0100

    Update export.rb

--- lib/icu/export.rb
+++ lib/icu/export.rb
@@ -5,8 +5,15 @@ module ICU
 
       # Run the command that creates both export zip files. Perl is used because
       # it isn't so easy to create DBF files (for SwissPerfect) using Ruby.
+      # The two file previously produced are swiss_perfect_pub.dbf and
+      # e.g. swiss_manager_pub.Jun19.txt suitable for previous versions of swiss_manager
+      # JL added another file swiss_manager_unicode_pub.MMMYY.xls suitable for swiss_manager from 2019 on
+      # All bundled in a file called pub_ratings.zip (see line 31 below) 
+      # available from https://ratings.icu.ie/downloads currently(2019) as admin
+      # This page has one live_ratings.zip and lots of pub_ratings.zip s
       cmd = "bin/export.pl -e #{Rails.env} 2>&1"
       out = `#{cmd}`.strip
+      # .strip removes only leading and trailing whitespace
 
       # Initialise a report and a Boolean success for the Event we'll create later.
       report = []
@@ -15,13 +22,17 @@ module ICU
       report.push out
       report.push "---"
       success = true
+      # report should be [export from perl, ---, export from perl(stripped), ---, updated/created, published, 
 
       # Update (or create) a Download object for each export file.
       begin
         %w(published live).each do |type|
+          # ternary operator. If Condition is true ? Then value X : Otherwise value Y
           short = type == "published" ? "pub" : "live"
-          file  = "tmp/#{short}.zip"
+          file  = "tmp/#{short}.zip"   # e.g. tmp/pub_ratings.zip
           match = "wrote #{file}"
+          # if the index of "wrote tmp/pub_ratings.zip" returns true i.e. if the element "wrote tmp/pub_ratings.zip"
+          # is in the stripped output from perl
           if out.index(match)
             comment      = "Latest #{type} ratings"
             file_name    = "#{short}_ratings.zip"
@@ -32,6 +43,7 @@ module ICU
               rating_list = nil
             end
             data         = File.open(file, "r", encoding: "ASCII-8BIT") { |f| f.read }
+            # creates a Download object with attributes as shown
             download = Download.where(comment: comment, file_name: file_name, content_type: content_type, rating_list: rating_list).first
             if download
               action = "updated"
